08f8fe8cbfd09281a6e552201414505b84abd2ed
jet <jet.chen@wailianvisa.com>
2019-04-04 09:33:17 +0800

    repository create

---
+++ src/main/java/com/jet/jim/JimApplication.java
@@ -0,0 +1,13 @@
+package com.jet.jim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JimApplication.class, args);
+    }
+
+}
