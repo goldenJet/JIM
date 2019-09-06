0338b98c31bce3df5899d3e46dddcf102f00c2e8
jet.chen <chen_kaikai777@126.com>
2019-09-06 22:50:37 +0800

    module update

---
+++ jim-client-java/src/main/java/com/jet/client/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.jet.client;
+
+/**
+ * @ClassName: HelloWorld
+ * @Description:
+ * @Author: Jet.Chen
+ * @Date: 2019/9/6 22:44
+ * @Version: 1.0
+ **/
+public class HelloWorld {
+}
---
+++ jim-common/src/main/java/com/jet/common/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.jet.common;
+
+/**
+ * @ClassName: HelloWorld
+ * @Description:
+ * @Author: Jet.Chen
+ * @Date: 2019/9/6 22:45
+ * @Version: 1.0
+ **/
+public class HelloWorld {
+}
