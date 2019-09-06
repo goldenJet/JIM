924636c5c074ac0dd2ff3a37aa19bdd0cfbee103
jet.chen <chen_kaikai777@126.com>
2019-09-06 22:40:29 +0800

    module update

---
+++ jim-common/src/test/java/com/jet/common/JimCommonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jet.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JimCommonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
--- jim-server/src/main/java/com/jet/server/JimApplication.java
+++ jim-server/src/main/java/com/jet/server/JimApplication.java
@@ -1,13 +1,19 @@
-package com.jet.jim;
+package com.jet.server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JimApplication {
+public class JimApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(JimApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
     }
 
 }
